cb8f7c687c5022caf6cf983de664d897c8500bec
Laura Moreby <lauramoreby@glaroam2-186-64.wireless.gla.ac.uk>
2017-02-06 18:19:59 +0000

    Completed up to end of Chapter 3

--- rango/views.py
+++ rango/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse #import the HttpResponse object from the django.http module
 
 def index(request): #Each view takes in at least one argument - a HttpRequest object (lives in django.http module). Named request by convention. 
-	return HttpResponse("Rango says hey there partner!") #each view must return a HttpResponse object
+	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>") #each view must return a HttpResponse object
 	#A simple HttpResponse object takes a string parameter representing the content of the page we wish to send to the client requesting the view
+
+	
+def about(request):
+	return HttpResponse("Rango says here is the about page! <br/> <a href='/rango/'>Index</a>")
