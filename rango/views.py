742f72d49b412b7149078a64d14ee66c94e3142a
Laura Moreby <lauramoreby@Lauras-MacBook-Pro.local>
2017-02-08 13:27:00 +0000

    Completed up to end of Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse #import the HttpResponse object from the django.http module
 
-def index(request): #Each view takes in at least one argument - a HttpRequest object (lives in django.http module). Named request by convention. 
-	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>") #each view must return a HttpResponse object
-	#A simple HttpResponse object takes a string parameter representing the content of the page we wish to send to the client requesting the view
+#Each view takes in at least one argument - a HttpRequest object (lives in django.http module). Named request by convention.
+#Each view must return a HttpResponse object
+#A simple HttpResponse object takes a string parameter representing the content of the page we wish to send to the client requesting the view
 
-	
+
+def index(request):
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"} # Construct a dictionary to pass to the template engine as its context. Note the key boldmessage is the same as {{ boldmessage }} in the template!
+	return render(request, 'rango/index.html', context=context_dict)
+	# Return a rendered response to send to the client.
+	# We make use of the shortcut function to make our lives easier. Note that the first parameter is the template we wish to use.
+
+
 def about(request):
-	return HttpResponse("Rango says here is the about page! <br/> <a href='/rango/'>Index</a>")
+	return render(request, 'rango/about.html', context={})
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,9 +20,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [ #python list
 	url(r'^$', views.index, name='index'),  #maps the basic URL to the index view in the rango application
 	url(r'^rango/', include('rango.urls')), # maps any URLs starting with rango/ to be handled by the rango application
-    url(r'^admin/', admin.site.urls),   
-]
+    url(r'^admin/', admin.site.urls), ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
