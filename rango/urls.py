ee440515f5055618c463fd625a0caba963dd5f16
Laura Moreby <lauramoreby@glaroam2-141-33.wireless.gla.ac.uk>
2017-02-09 17:59:04 +0000

    Completed up to end of Chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,7 @@ from rango import views #imports the views module from rango
 urlpatterns = [
     url(r'^$', views.index, name='index'), #handles the remaining URL string and map the empty string to the index view
     url(r'^about/',views.about,name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
